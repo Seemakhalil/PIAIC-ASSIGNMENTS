6c51c76f30587fe5bbd546217535959a6d8e17df
Seemakhalil <50095539+Seemakhalil@users.noreply.github.com>
2019-05-30 12:45:58 +0500

    task 2

---
+++ task2 check num - + or0.py
@@ -0,0 +1,8 @@
+# check whether number is positive, negative or zero
+num=float(input("Enter a number: "))
+if num >0 :
+    print("Number is positive:")
+elif num<0:
+    print("Number is negative")
+else:
+    print("Number is zero")
